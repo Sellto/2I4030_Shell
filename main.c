69bf18c81e6f1d32a7240aca489fcb1629bfcb72
sellto <selleslagh.tom@gmail.com>
2017-12-13 10:05:46 +0000

    Personnalise main.c

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
-/***************************************************************************//**
-  @file         main.c
-  @author       Stephen Brennan
-  @date         Thursday,  8 January 2015
-  @brief
-*******************************************************************************/
+//The shell's core is inspired by the work of S.Brennan.
 
 #include <stdio.h>
 #include <sys/wait.h>
@@ -12,43 +7,46 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function Declarations for builtin shell commands :
+
+
+// Function Declarations
 int rpi_exit(char **args);
 int rpi_hello(char **args);
 int rpi_raspInfo(char **args);
+int rpi_clear(char **args);
+int rpi_help(char **args);
 
-// List of builtin commands, followed by their corresponding functions
-char *builtin_str[] = {
-        "exit",
-        "hello",
-        "rasp"
-};
-
+//Pointer to the functions
 int (*builtin_func[]) (char **) = {
         &rpi_exit,
         &rpi_hello,
         &rpi_raspInfo,
+        &rpi_clear,
+        &rpi_help
+};
+
+// Function "string call"
+char *builtin_str[] = {
+        "exit",
+        "hello",
+        "rasp",
+        "cls",
+        "help"
 };
 
 int rpi_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
 
-// Builtin function implementations
-/**
-   @brief Bultin command: change directory.
-   @param args List of args.  args[0] is "cd".  args[1] is the directory.
-   @return Always returns 1, to continue executing.
-*/
 
+//Functions implementation.
 int rpi_raspInfo(char **args)
 {
   // /proc/self/status
   // top
   system("top");
-
+    return 1;
 }
-
 
 int rpi_hello(char **args)
 {
@@ -56,27 +54,28 @@ int rpi_hello(char **args)
   return 1;
 }
 
-/**
-   @brief Builtin command: exit.
-   @param args List of args.  Not examined.
-   @return Always returns 0, to terminate execution.
- */
+int rpi_clear(char **args)
+{
+    system("clear");
+    return 1;
+}
+
+int rpi_help(char **args)
+{
+    printf("Help\n");
+    return 1;
+}
+
 int rpi_exit(char **args)
 {
   return 0;
 }
 
-/**
-  @brief Launch a program and wait for it to terminate.
-  @param args Null terminated list of arguments (including program).
-  @return Always returns 1, to continue execution.
- */
-
+//Shell Core
 int rpi_launch(char **args)
 {
   pid_t pid;
   int status;
-
   pid = fork();
   if (pid == 0) {
     // Child process
@@ -93,45 +92,34 @@ int rpi_launch(char **args)
       waitpid(pid, &status, WUNTRACED);
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
   }
-
   return 1;
 }
 
-/**
-   @brief Execute shell built-in or launch program.
-   @param args Null terminated list of arguments.
-   @return 1 if the shell should continue running, 0 if it should terminate
- */
+
+
 int rpi_execute(char **args)
 {
   int i;
-
   if (args[0] == NULL) {
     // An empty command was entered.
     return 1;
   }
-
   for (i = 0; i < rpi_num_builtins(); i++) {
     if (strcmp(args[0], builtin_str[i]) == 0) {
       return (*builtin_func[i])(args);
     }
   }
-
   return rpi_launch(args);
 }
 
+//Pick the user entry.
 #define rpi_RL_BUFSIZE 1024
-/**
-   @brief Read a line of input from stdin.
-   @return The line from stdin.
- */
 char *rpi_read_line(void)
 {
   int bufsize = rpi_RL_BUFSIZE;
   int position = 0;
   char *buffer = malloc(sizeof(char) * bufsize);
   int c;
-
   if (!buffer) {
     fprintf(stderr, "rpi: allocation error\n");
     exit(EXIT_FAILURE);
@@ -140,7 +128,6 @@ char *rpi_read_line(void)
   while (1) {
     // Read a character
     c = getchar();
-
     if (c == EOF) {
       exit(EXIT_SUCCESS);
     } else if (c == '\n') {
@@ -163,29 +150,23 @@ char *rpi_read_line(void)
   }
 }
 
+
+//"Decode" the user entry.
 #define rpi_TOK_BUFSIZE 64
 #define rpi_TOK_DELIM " \t\r\n\a"
-/**
-   @brief Split a line into tokens (very naively).
-   @param line The line.
-   @return Null-terminated array of tokens.
- */
 char **rpi_split_line(char *line)
 {
   int bufsize = rpi_TOK_BUFSIZE, position = 0;
   char **tokens = malloc(bufsize * sizeof(char*));
   char *token, **tokens_backup;
-
   if (!tokens) {
     fprintf(stderr, "rpi: allocation error\n");
     exit(EXIT_FAILURE);
   }
-
   token = strtok(line, rpi_TOK_DELIM);
   while (token != NULL) {
     tokens[position] = token;
     position++;
-
     if (position >= bufsize) {
       bufsize += rpi_TOK_BUFSIZE;
       tokens_backup = tokens;
@@ -196,16 +177,15 @@ char **rpi_split_line(char *line)
         exit(EXIT_FAILURE);
       }
     }
-
     token = strtok(NULL, rpi_TOK_DELIM);
   }
   tokens[position] = NULL;
   return tokens;
 }
 
-/**
-   @brief Loop getting input and executing it.
- */
+//SuperLoop
+#define KBLU  "\x1B[32m"
+#define RESET "\x1B[0m"
 void rpi_loop(void)
 {
   char *line;
@@ -213,31 +193,19 @@ void rpi_loop(void)
   int status;
 
   do {
-    printf("> ");
+    printf(KBLU" RPi-:> "RESET);
     line = rpi_read_line();
     args = rpi_split_line(line);
     status = rpi_execute(args);
-
     free(line);
     free(args);
   } while (status);
 }
 
-/**
-   @brief Main entry point.
-   @param argc Argument count.
-   @param argv Argument vector.
-   @return status code
- */
 int main(int argc, char **argv)
 {
-  // Load config files, if any.
-
-  // Run command loop.
-  rpi_loop();
-
-  // Perform any shutdown/cleanup.
-
+    system("clear");
+    rpi_loop();
+    system("clear");
   return EXIT_SUCCESS;
 }
-
