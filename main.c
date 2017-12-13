49f84787c58fa1744c44e9f64efb7b895895e9b9
sellto <selleslagh.tom@gmail.com>
2017-12-13 08:24:53 +0000

    Change function name

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
   @file         main.c
   @author       Stephen Brennan
   @date         Thursday,  8 January 2015
-  @brief        LSH (Libstephen SHell)
+  @brief
 *******************************************************************************/
 
 #include <stdio.h>
@@ -13,27 +13,24 @@
 #include <string.h>
 
 // Function Declarations for builtin shell commands :
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
-int lsh_hello(char **args);
+int rpi_exit(char **args);
+int rpi_hello(char **args);
+int rpi_raspInfo(char **args);
 
 // List of builtin commands, followed by their corresponding functions
 char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit",
-  "hello"
+        "exit",
+        "hello",
+        "rasp"
 };
 
 int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit,
-  &lsh_hello
+        &rpi_exit,
+        &rpi_hello,
+        &rpi_raspInfo,
 };
 
-int lsh_num_builtins() {
+int rpi_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
 
@@ -43,47 +40,20 @@ int lsh_num_builtins() {
    @param args List of args.  args[0] is "cd".  args[1] is the directory.
    @return Always returns 1, to continue executing.
 */
-int lsh_cd(char **args)
+
+int rpi_raspInfo(char **args)
 {
-  if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("lsh");
-    }
-  }
-  return 1;
+  // /proc/self/status
+  // top
+  system("top");
+
 }
 
-/**
-   @brief Builtin command: print help.
-   @param args List of args.  Not examined.
-   @return Always returns 1, to continue executing.
- */
-int lsh_help(char **args)
+
+int rpi_hello(char **args)
 {
-  int i;
-  printf("Stephen Brennan's LSH\n");
-  printf("Type program names and arguments, and hit enter.\n");
-  printf("The following are built in:\n");
-
-  for (i = 0; i < lsh_num_builtins(); i++) {
-    printf("  %s\n", builtin_str[i]);
-  }
-
-  printf("Use the man command for information on other programs.\n");
+  printf("Hello World ! \n");
   return 1;
-}
-
-/**
-   @brief Builtin command: print hello
-   @param args List of args.  Not examined.
-   @return Always returns 1, to continue executing.
- */
-int lsh_hello(char **args)
-{
-    printf("Hello World !");
-    return 1;
 }
 
 /**
@@ -91,7 +61,7 @@ int lsh_hello(char **args)
    @param args List of args.  Not examined.
    @return Always returns 0, to terminate execution.
  */
-int lsh_exit(char **args)
+int rpi_exit(char **args)
 {
   return 0;
 }
@@ -102,7 +72,7 @@ int lsh_exit(char **args)
   @return Always returns 1, to continue execution.
  */
 
-int lsh_launch(char **args)
+int rpi_launch(char **args)
 {
   pid_t pid;
   int status;
@@ -111,12 +81,12 @@ int lsh_launch(char **args)
   if (pid == 0) {
     // Child process
     if (execvp(args[0], args) == -1) {
-      perror("lsh");
+      perror("rpi");
     }
     exit(EXIT_FAILURE);
   } else if (pid < 0) {
     // Error forking
-    perror("lsh");
+    perror("rpi");
   } else {
     // Parent process
     do {
@@ -132,7 +102,7 @@ int lsh_launch(char **args)
    @param args Null terminated list of arguments.
    @return 1 if the shell should continue running, 0 if it should terminate
  */
-int lsh_execute(char **args)
+int rpi_execute(char **args)
 {
   int i;
 
@@ -141,29 +111,29 @@ int lsh_execute(char **args)
     return 1;
   }
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
+  for (i = 0; i < rpi_num_builtins(); i++) {
     if (strcmp(args[0], builtin_str[i]) == 0) {
       return (*builtin_func[i])(args);
     }
   }
 
-  return lsh_launch(args);
+  return rpi_launch(args);
 }
 
-#define LSH_RL_BUFSIZE 1024
+#define rpi_RL_BUFSIZE 1024
 /**
    @brief Read a line of input from stdin.
    @return The line from stdin.
  */
-char *lsh_read_line(void)
+char *rpi_read_line(void)
 {
-  int bufsize = LSH_RL_BUFSIZE;
+  int bufsize = rpi_RL_BUFSIZE;
   int position = 0;
   char *buffer = malloc(sizeof(char) * bufsize);
   int c;
 
   if (!buffer) {
-    fprintf(stderr, "lsh: allocation error\n");
+    fprintf(stderr, "rpi: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
@@ -183,51 +153,51 @@ char *lsh_read_line(void)
 
     // If we have exceeded the buffer, reallocate.
     if (position >= bufsize) {
-      bufsize += LSH_RL_BUFSIZE;
+      bufsize += rpi_RL_BUFSIZE;
       buffer = realloc(buffer, bufsize);
       if (!buffer) {
-        fprintf(stderr, "lsh: allocation error\n");
+        fprintf(stderr, "rpi: allocation error\n");
         exit(EXIT_FAILURE);
       }
     }
   }
 }
 
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
+#define rpi_TOK_BUFSIZE 64
+#define rpi_TOK_DELIM " \t\r\n\a"
 /**
    @brief Split a line into tokens (very naively).
    @param line The line.
    @return Null-terminated array of tokens.
  */
-char **lsh_split_line(char *line)
+char **rpi_split_line(char *line)
 {
-  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  int bufsize = rpi_TOK_BUFSIZE, position = 0;
   char **tokens = malloc(bufsize * sizeof(char*));
   char *token, **tokens_backup;
 
   if (!tokens) {
-    fprintf(stderr, "lsh: allocation error\n");
+    fprintf(stderr, "rpi: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
-  token = strtok(line, LSH_TOK_DELIM);
+  token = strtok(line, rpi_TOK_DELIM);
   while (token != NULL) {
     tokens[position] = token;
     position++;
 
     if (position >= bufsize) {
-      bufsize += LSH_TOK_BUFSIZE;
+      bufsize += rpi_TOK_BUFSIZE;
       tokens_backup = tokens;
       tokens = realloc(tokens, bufsize * sizeof(char*));
       if (!tokens) {
-		free(tokens_backup);
-        fprintf(stderr, "lsh: allocation error\n");
+        free(tokens_backup);
+        fprintf(stderr, "rpi: allocation error\n");
         exit(EXIT_FAILURE);
       }
     }
 
-    token = strtok(NULL, LSH_TOK_DELIM);
+    token = strtok(NULL, rpi_TOK_DELIM);
   }
   tokens[position] = NULL;
   return tokens;
@@ -236,7 +206,7 @@ char **lsh_split_line(char *line)
 /**
    @brief Loop getting input and executing it.
  */
-void lsh_loop(void)
+void rpi_loop(void)
 {
   char *line;
   char **args;
@@ -244,9 +214,9 @@ void lsh_loop(void)
 
   do {
     printf("> ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
+    line = rpi_read_line();
+    args = rpi_split_line(line);
+    status = rpi_execute(args);
 
     free(line);
     free(args);
@@ -264,7 +234,7 @@ int main(int argc, char **argv)
   // Load config files, if any.
 
   // Run command loop.
-  lsh_loop();
+  rpi_loop();
 
   // Perform any shutdown/cleanup.
 
